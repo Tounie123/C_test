f0b41bd5f8b04d9584feb522f39f26557932b647
zhou tuan <zhoutuan@zhoudeair.lan>
2018-12-28 20:21:52 +0800

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by zhou tuan on 2018/12/28.
+//  Copyright © 2018年 zhou tuan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
